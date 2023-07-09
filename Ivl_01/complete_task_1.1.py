b72bcd99841aaf7b15191f8f8960fda5fb833e36
MaryKuzn <Kuznetsovam.a@mail.ru>
2023-07-09 22:47:44 +0300

    Complete_task_1.1.py изменен способ 2, del способ 3

--- Ivl_01/complete_task_1.1.py
+++ Ivl_01/complete_task_1.1.py
@@ -6,8 +6,7 @@ print((my_favorite_songs[:14] +
        my_favorite_songs[23:30] + 
        my_favorite_songs[50:62]))
 # 2 способ
-#Name = "Waste a Moment" " " "New Salvation" " " "Stayin' Alive" " " "Start Me Up" 
-#print(my_favorite_songs.format(Name))
-# 3 способ
-#my_favorite_songs = ['Waste a Moment', 'Stayin\' Alive', 'A Sorta Fairytale', 'Start Me Up', 'New Salvation']
-#print(my_favorite_songs[0] , my_favorite_songs[-1], my_favorite_songs[1], my_favorite_songs[-2])
+print(my_favorite_songs.split(sep=',')[0],
+      my_favorite_songs.split(sep=',')[-1],
+      my_favorite_songs.split(sep=',')[1],
+      my_favorite_songs.split(sep=',')[-2])
