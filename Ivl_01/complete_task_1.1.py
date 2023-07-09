35c3be4be8a4733bcaaaa15c1caf02cfadb796ce
MaryKuzn <Kuznetsovam.a@mail.ru>
2023-07-09 15:20:21 +0300

    coplete_task_1.1.py создан в Ivl_01

---
+++ Ivl_01/complete_task_1.1.py
@@ -0,0 +1,12 @@
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+print(my_favorite_songs[:14])
+print(my_favorite_songs[-13:])
+print(my_favorite_songs.find("S"))
+print(my_favorite_songs[16:22] + my_favorite_songs[23:31])
+print(my_favorite_songs.rfind("U"))
+print(my_favorite_songs[50:62])
+print((my_favorite_songs[:14] + 
+       my_favorite_songs[-14:] + 
+       my_favorite_songs[15:22] + 
+       my_favorite_songs[23:30] + 
+       my_favorite_songs[50:62]))
